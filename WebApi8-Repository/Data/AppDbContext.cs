e1fc9dbdddf40ed5c33d40932c9261037cd23423
Ana Júlia <aanajulia505@gmail.com>
2025-10-02 14:39:24 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi8-Repository/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi8_EF.Models;
+
+namespace WebApi8_EF.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
+
+    }
+}
